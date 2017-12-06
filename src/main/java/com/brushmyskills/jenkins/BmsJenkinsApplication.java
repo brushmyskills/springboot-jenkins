2aa593b551f49c9d5c7a4748d7544cfc24798d80
BrushMySkills <brushmyskills@gmail.com>
2017-12-06 17:12:26 +0530

    Commit to test Jenkins build

---
+++ src/main/java/com/brushmyskills/jenkins/BmsJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.brushmyskills.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmsJenkinsApplication.class, args);
+	}
+}
+
+
---
+++ src/test/java/com/brushmyskills/jenkins/BmsJenkinsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brushmyskills.jenkins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BmsJenkinsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
